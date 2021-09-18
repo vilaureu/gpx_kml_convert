bfd69a60c695979c45fe70a84fda3321b0a4e4eb
vilaureu <vilaureu@users.noreply.github.com>
2021-09-18 12:25:56 +0200

    getting started

---
+++ gpx2kml/examples/valid.rs
@@ -0,0 +1,12 @@
+use std::{fs::File, io::stdout, path::Path};
+
+use gpx2kml::convert;
+
+const RESOURCES: &str = "./resources/";
+
+fn main() {
+    let mut source =
+        File::open(Path::new(RESOURCES).join("valid.gpx")).expect("valid.gpx not found");
+    let mut sink = stdout();
+    convert(&mut source, &mut sink);
+}
