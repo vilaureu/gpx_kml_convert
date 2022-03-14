dd837d1f42401ee711386cf1af1c4283b3b7a1cc
vilaureu <vilaureu@users.noreply.github.com>
2022-03-14 09:54:44 +0100

    create wasm crate using kml library
    This creates a wasm crate in the workspace and adds the kml library
    to the dependencies. This library currently needs the kml.patch applied.

--- gpx2kml/src/lib.rs
+++ gpx2kml/src/lib.rs
@@ -1,23 +1,5 @@
 use std::io::{Read, Write};
 
-use gpx::Gpx;
-use xml::{writer::XmlEvent, EmitterConfig, EventWriter};
-
 pub fn convert(source: &mut impl Read, sink: &mut impl Write) {
     let gpx = gpx::read(source).unwrap();
-    let writer = EmitterConfig::new()
-        .perform_indent(true)
-        .keep_element_names_stack(false)
-        .create_writer(sink);
-}
-
-trait WriteKML {
-    fn write(writer: &mut EventWriter<impl Write>);
-}
-
-impl WriteKML for Gpx {
-    fn write(writer: &mut EventWriter<impl Write>) {
-        let start = XmlEvent::start_element("kml").default_ns("http://www.opengis.net/kml/2.2");
-        writer.write(start).unwrap();
-    }
 }
